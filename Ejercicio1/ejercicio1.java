f8094861a94a95e4f3c0d3ce145d778b719a006b
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-10-25 00:20:05 -0500

    probando dijkstra en el main

--- Ejercicio1/ejercicio1.java
+++ Ejercicio1/ejercicio1.java
@@ -2,7 +2,19 @@ package Ejercicio1;
 
 public class ejercicio1 {
     public static void main(String[] args) {
+        Graph g = new Graph();
 
+        // Generar un grafo aleatorio con 5 nodos, 8 aristas, y con pesos
+        g.generarGrafoAleatorio(8, 15, true);
+
+        // Mostrar el grafo generado
+        System.out.println("Grafo Generado:");
+        g.showGraph();
+        
+        System.out.println("\n"); // Espacio
+
+        // Ejecutar Dijkstra comenzando desde el nodo "N0"
+        g.dijkstra("N0");
     }
 }
 
